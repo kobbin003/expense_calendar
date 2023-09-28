0b293737d782ddf508feb00e563c65c96b1cd8e9
kobbin003 <dkfeto@gmail.com>
2023-09-28 20:01:41 +0530

    AuthForm component

---
+++ src/component/auth/AuthForm.tsx
@@ -0,0 +1,86 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import login from "../../firebase/auth/login";
+import AuthButton from "../authButton/AuthButton";
+import signUp from "../../firebase/auth/signUp";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../Reducer/userSlice";
+import { addUser } from "../../firestoreFns/addUser";
+import { useNavigate } from "react-router-dom";
+
+type UserFormType = { email: string; password: string };
+
+const AuthForm = ({ type }: { type: "Login" | "Signup" }) => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const [formData, setFormData] = useState<UserFormType>({
+		email: "",
+		password: "",
+	});
+
+	const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const inputName = e.target.name;
+		const inputValue = e.target.value;
+		setFormData((prev) => ({ ...prev, [inputName]: inputValue }));
+	};
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(formData);
+		if (type == "Login") {
+			login(formData).then((res) => {
+				const {
+					user: { displayName, email, photoURL, phoneNumber, uid },
+				} = res;
+				const userInfo = { displayName, email, photoURL, phoneNumber, uid };
+				//*  set the user in redux state
+				dispatch(setUser(userInfo));
+				//* navigate to app
+				navigate(`/${uid}`);
+			});
+		} else {
+			signUp(formData).then((res) => {
+				const {
+					user: { displayName, email, photoURL, phoneNumber, uid },
+				} = res;
+				const userInfo = { displayName, email, photoURL, phoneNumber, uid };
+				//*  add the user in firestore
+				addUser(userInfo);
+				//*  set the user in redux state
+				dispatch(setUser(userInfo));
+				//* navigate to app
+				navigate(`/${uid}`);
+			});
+		}
+	};
+
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className="form-control gap-2"
+		>
+			<input
+				type="email"
+				name="email"
+				value={formData.email}
+				placeholder="email"
+				onChange={handleOnChange}
+				className="border-2 focus:outline-none border-gray-400 rounded-sm p-1"
+			/>
+			<input
+				type="password"
+				name="password"
+				value={formData.password}
+				placeholder="password"
+				onChange={handleOnChange}
+				className="border-2 focus:outline-none border-gray-400 rounded-sm p-1"
+			/>
+			<AuthButton
+				bgColor="bg-orange-400"
+				img="/src/assets/mail.svg"
+				desc={type}
+			/>
+		</form>
+	);
+};
+
+export default AuthForm;
