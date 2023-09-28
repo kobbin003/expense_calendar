fcdc6bde5ba348210fa95478065b6d7d8e4ced54
kobbin003 <dkfeto@gmail.com>
2023-09-28 16:11:04 +0530

    firebase config

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyC-GowMxe3fr89nE1W5G6-qd7dwLq5V0Ew",
+	authDomain: "expense-calendar-8fc9f.firebaseapp.com",
+	projectId: "expense-calendar-8fc9f",
+	storageBucket: "expense-calendar-8fc9f.appspot.com",
+	messagingSenderId: "1009804816042",
+	appId: "1:1009804816042:web:e87a3914252f9ec838aad3",
+	measurementId: "G-50D1CWHPRF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
