1068f3202d04542c0033fc4df0c1293283e52f97
kobbin003 <dkfeto@gmail.com>
2023-10-02 12:36:58 +0530

    redux-persist added

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router.tsx";
+import { Provider } from "react-redux";
+import { persistor, store } from "./store/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<PersistGate
+				loading={null}
+				persistor={persistor}
+			>
+				<RouterProvider router={router} />
+			</PersistGate>
+		</Provider>
 	</React.StrictMode>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "../Reducer/userSlice";
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	REGISTER,
+	PURGE,
+	PERSIST,
+	PAUSE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+	key: "root",
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-	reducer: { user: userReducer },
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
