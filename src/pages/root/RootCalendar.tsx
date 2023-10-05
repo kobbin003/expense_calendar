c39cef07ffe860da5c932928f46bcf0d14e26c7e
kobbin003 <dkfeto@gmail.com>
2023-10-05 18:31:02 +0530

    root-calendar page

---
+++ src/pages/root/RootCalendar.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import CalendarNav from "../../component/CalendarNav";
+import MyCalendar from "../../component/myCalendar/MyCalendar";
+
+type Props = {};
+
+const RootCalendar = ({}: Props) => {
+	const now = new Date();
+	const [dateSelected, setDateSelected] = useState<Date>(now);
+	return (
+		<>
+			<CalendarNav
+				dateSelected={dateSelected}
+				setDateSelected={setDateSelected}
+			/>
+			<MyCalendar dateSelected={dateSelected} />
+		</>
+	);
+};
+
+export default RootCalendar;
