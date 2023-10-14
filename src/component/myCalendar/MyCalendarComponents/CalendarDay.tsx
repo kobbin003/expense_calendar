666da193b202899c58a219e5e4f3324b09f63db5
kobbin003 <dkfeto@gmail.com>
2023-10-14 20:41:05 +0530

    day expense value added

--- src/component/myCalendar/MyCalendarComponents/CalendarDay.tsx
+++ src/component/myCalendar/MyCalendarComponents/CalendarDay.tsx
@@ -1,27 +1,59 @@
 import { Link } from "react-router-dom";
-import { dateToParamsConverter } from "../../../utils/date/dateToParamConverter";
-import { isSameDate } from "../../../utils/date/isSameDate";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
+import { dateToString } from "../../../utils/date/date-fns/dateToString";
+import { useGetExpenseByDay } from "../../../hook/useGetExpenseByDay";
+import { useEffect, useState } from "react";
 
 type Props = {
 	date: Date;
 };
 
 const CalendarDay = ({ date }: Props) => {
-	const { uid } = useSelector((state: RootState) => state.user);
-	const param = dateToParamsConverter(date);
-	const now = new Date();
-	const dateIsToday = isSameDate(now, date);
+	const { uid, firestoreUserDocRef } = useSelector(
+		(state: RootState) => state.user
+	);
+
+	const param = dateToString(date);
+
+	const now = dateToString(new Date());
+
+	const dateIsToday = now == dateToString(date);
+
+	const [totalExpenseForDay, setTotalExpenseForDay] = useState(0);
+
+	const { data, isLoading } = useGetExpenseByDay(
+		param || "",
+		firestoreUserDocRef || ""
+	);
+
+	useEffect(() => {
+		let total = 0;
+		data?.forEach((item) => {
+			const expense = item.data();
+			total += expense.amount;
+		});
+		setTotalExpenseForDay(total);
+	}, [data]);
 
 	return (
 		<Link
 			to={`/${uid}/${param}`}
 			className={`border border-gray-200 shadow-lg hover:scale-105 hover:bg-gray-200 p-2 ${
-				dateIsToday ? "bg-gray-200 text-3xl sm:text-4xl" : "text-gray-800"
-			}`}
+				dateIsToday ? "bg-gray-200" : "text-gray-800"
+			} flex flex-col md:flex-row justify-between`}
 		>
-			{date.getDate()}
+			<p className={`${dateIsToday ? " text-3xl sm:text-4xl" : ""}`}>
+				{date.getDate()}
+			</p>
+
+			<div className="flex flex-col justify-end">
+				{isLoading ? (
+					<span className="loading loading-dots loading-xs"></span>
+				) : (
+					<span>{totalExpenseForDay}</span>
+				)}
+			</div>
 		</Link>
 	);
 };
