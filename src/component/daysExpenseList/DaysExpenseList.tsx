798e1391cca3b109ce9cdbbd30f1779834b65b90
kobbin003 <dkfeto@gmail.com>
2023-10-24 19:53:13 +0530

    amount currency formatted

--- src/component/daysExpenseList/DaysExpenseList.tsx
+++ src/component/daysExpenseList/DaysExpenseList.tsx
@@ -1,38 +1,16 @@
-import { QueryDocumentSnapshot } from "firebase/firestore";
-import { format } from "date-fns";
+import { useFormatCurrency } from "../../hook/useFormatCurrency";
+import { ExpenseType } from "../../types/expense";
 
 type Props = {
-	expenses: QueryDocumentSnapshot[];
+	expense: ExpenseType;
+	time: string;
 };
 
-const DaysExpenseList = ({ expenses }: Props) => {
-	/** fetch all the expenses for current day */
+export const DaysExpenseList = ({ expense, time }: Props) => {
+	const expenseAmountWithCurrency = useFormatCurrency(expense.amount);
 	return (
-		<div>
-			<ul className="list-none">
-				{expenses.length > 0 ? (
-					expenses.map((item) => {
-						const expense = item.data();
-						const time = format(
-							expense.expenseDate.toDate(),
-							"hh: mm b"
-							// "hh'hr':mm'min':ss'sec' b"
-						);
-						return (
-							<li
-								key={item.id}
-								className="border-b-2 w-max"
-							>
-								you spent {expense.amount} on {expense.description} at {time}
-							</li>
-						);
-					})
-				) : (
-					<p>No expenses made on this day</p>
-				)}
-			</ul>
-		</div>
+		<li className="border-b-2 w-max">
+			you spent {expenseAmountWithCurrency} on {expense.description} at {time}
+		</li>
 	);
 };
-
-export default DaysExpenseList;
