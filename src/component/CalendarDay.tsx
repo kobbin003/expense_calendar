283c37ec4aa2a89ca480e322da9aa0ab5f8fc211
kobbin003 <dkfeto@gmail.com>
2023-11-01 19:16:50 +0530

    deletes

--- src/component/CalendarDay.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-	content: string | number;
-};
-
-const CalendarDay = ({ content }: Props) => {
-	return <div className="border border-white bg-orange-600">{content}</div>;
-};
-
-export default CalendarDay;
--- src/utils/date/date-fns/dateToString.ts
+++
@@ -1,5 +0,0 @@
-import { format } from "date-fns";
-
-export const dateToString = (date: Date) => {
-	return format(date, "yyyyMMdd");
-};
