31e72df97b857dc9a515f6ae7eacebe6f3690375
kobbin003 <dkfeto@gmail.com>
2023-11-11 12:48:48 +0530

    deletes

--- src/utils/date/dateToParamConverter.ts
+++
@@ -1,6 +0,0 @@
-import { getDateTimeComponents } from "./getDateTimeComponents";
-
-export const dateToParamsConverter = (date: Date) => {
-	const dateComps = getDateTimeComponents(date);
-	return `${dateComps.day}${dateComps.month}${dateComps.year}`;
-};
